4dd7140fb3391656ecac1e4aceb6d2fcfd808cf7
Dennis Chavez Romero <dennis.chavezromero@hotmail.com>
2026-03-21 23:01:50 +0100

    Include other sample vids from ffirenet.

--- YOLO_detection.py
+++ YOLO_detection.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO("models/26m_1280imgsz_200epochs/weights/best.pt") # choose model to use from models folders
 
 results = model.predict(
-    source="sample_videos/fire2.mp4",
+    source="sample_videos/fire3.mp4",
     conf=0.3,
     show=True,
     save=False,
