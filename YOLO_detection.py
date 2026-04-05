437114e26147820f9192dca620d5c37bc2949c51
Dennis Chavez Romero <dennis.chavezromero@hotmail.com>
2026-04-05 22:42:24 +0200

    Updating selected model for detection scripts and adding latest vids from main branch.

--- YOLO_detection.py
+++ YOLO_detection.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("models/26m_1280imgsz_200epochs/weights/best.pt") # choose model to use from models folders
+model = YOLO("models/26m_640imgsz_200epochs/weights/best.pt") # choose model to use from models folders
 
 results = model.predict(
-    source="sample_videos/fire3.mp4",
+    source="sample_videos/fire7.mp4",
     conf=0.3,
     show=True,
     save=False,
